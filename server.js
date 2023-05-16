2edf9cc080657e0158eeb3d20763a2988319e69d
abisheik88 <abishekvijayarangan@gmail.com>
2023-05-16 17:15:49 +0530

    user authentication

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const dbConnection = require("./db")
 const port = process.env.PORT || 5000;
 const cors = require('cors')
+app.use(express.json())
 
 app.use(cors())
 app.use("/api/cars", require('./routes/carsRoute'))
+app.use("/api/users", require('./routes/usersRoute'))
 app.get("/", (req, res) => {
     res.send("Server Running");
 });
